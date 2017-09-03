59e999bcfa912eee008579f45e7b74ef8593220d
mxdi9i7 <peter.zheng88228@gmail.com>
2017-09-02 20:14:25 -0400

    search works

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -6,45 +6,77 @@ class Search extends Component {
     constructor() {
         super()
         this.state = {
-            data: []
+            data: [],
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     componentDidMount() {
-        axios.get(`http://localhost:3001/api/james`).then((data) => {
+        axios.get(`http://localhost:3001/api/a`).then((data) => {
             this.setState({data: data.data.results})
         })
     }
+    
+    handleChange(e) {
+        const query = e.target.value
+        if (!e.target.value) {
+            axios.get('http://localhost:3001/api/a' + query).then((data) => {
+                this.setState({data: data.data.results})
+            })
+            this.setState({queryName: query})
+        }
+        axios.get('http://localhost:3001/api/' + query).then((data) => {
+            this.setState({data: data.data.results})
+        })
+        this.setState({queryName: query})
+    }
+    handleSubmit() {
+        const query = this.state.queryName
+        if (!this.state.queryName) {
+            axios.get('http://localhost:3001/api/a' + query).then((data) => {
+                this.setState({data: data.data.results, queryName: ""})
+            })
+        }
+        axios.get('http://localhost:3001/api/' + query).then((data) => {
+            this.setState({data: data.data.results, queryName: ""})
+        })
+    }
     render() {
-        console.log(this.state.data)
         const resultList = this.state.data;
         const baseUrl = "http://image.tmdb.org/t/p/w185/";
-        for (var i =0; i < resultList.length; i++) {
-            var self = resultList[i];
-            self.full_path = `${baseUrl}${self.profile_path}`
+        if (resultList) {
+            for (var i =0; i < resultList.length; i++) {
+                var self = resultList[i];
+                if (self.profile_path == null) {
+                    self.full_path = `https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png`
+                } else {
+                    self.full_path = `${baseUrl}${self.profile_path}`;
+                }
+                
+            }
         }
-        console.log(resultList)
         return (
             <div className="searchBar">
                 <div className="form-group">
                     <label className="text-align-left">Enter a movie star's name:</label>
-                    <input className="form-control searchInput"  placeholder="Enter a movie star's name here" />
+                    <input className="form-control searchInput" value={this.state.queryName} onChange={this.handleChange} placeholder="Enter a movie star's name here" />
+                    <button onClick={this.handleSubmit}>Search</button>
                 </div>
-                { resultList.map((item, index) => (
-                        
+                { resultList ? resultList.map((item, index) => (
                         <div className="searchResults" key={index}>
                             <div className="resultItem">
                                 <div className="imgContainer">
                                     <img src={item.full_path} alt="" />
                                 </div>
                                 <h1>Name: {item.name}</h1>
-                                <p>Most Famous For: {item.known_for[1].title}, {item.known_for[1].release_date}</p>
                             </div>
                         </div>
-                    ))
+                    )) : <h1>Your search did not return any result, please try a valid name.</h1>
                 }
                 
             </div>
         );
+       
     }
 }
 
